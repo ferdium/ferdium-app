481f5d3eefc9d965c22aeaf4fcc15aad4ed61399
Amine Mouafik <amine@mouafik.fr>
2020-09-17 17:15:08 +0100

    Add Belarusian to app locales

--- src/i18n/languages.js
+++ src/i18n/languages.js
@@ -25,7 +25,8 @@ export const APP_LOCALES = {
   tr: 'Türkçe',
   'zh-TW': '台語 (Chinese Taiwan)',
   es: 'Español',
-  no: 'Norsk (Norwegian)'
+  no: 'Norsk (Norwegian)',
+  be: 'белар (Belarusian)'
 };
 
 // Hunspell compatible keys
