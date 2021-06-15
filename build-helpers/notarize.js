40d14fa9f5edd8eb71f697eb8fe412bd0cd35bfd
Vijay A <avijayr@protonmail.com>
2021-06-15 08:27:19 +0530

    Skip notarization of the build artifact if not building for the main repo (ie building on a forked repo)..

--- build-helpers/notarize.js
+++ build-helpers/notarize.js
@@ -1,18 +1,18 @@
-const { notarize } = require("electron-notarize");
+const { notarize } = require('electron-notarize');
 
 exports.default = async function notarizing(context) {
   const { electronPlatformName, appOutDir } = context;
-  if (electronPlatformName !== "darwin" || (process.env.GIT_BRANCH_NAME !== 'release' && process.env.GIT_BRANCH_NAME !== 'nightly')) {
+  if (electronPlatformName !== 'darwin' || process.env.SKIP_NOTARIZATION === 'true' || (process.env.GIT_BRANCH_NAME !== 'release' && process.env.GIT_BRANCH_NAME !== 'nightly')) {
     return;
   }
 
   const appName = context.packager.appInfo.productFilename;
 
-  return await notarize({
-    appBundleId: "com.kytwb.ferdi",
+  await notarize({
+    appBundleId: 'com.kytwb.ferdi',
     appPath: `${appOutDir}/${appName}.app`,
-    ascProvider: "B6J9X9DWFL",
+    ascProvider: 'B6J9X9DWFL',
     appleId: process.env.APPLEID,
-    appleIdPassword: process.env.APPLEID_PASSWORD
+    appleIdPassword: process.env.APPLEID_PASSWORD,
   });
 };
