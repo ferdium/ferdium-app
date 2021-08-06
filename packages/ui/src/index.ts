3c02c9fd9067ef11d1f324ee0a15bb3a9de47517
Vijay Raghavan Aravamudhan <vraravam@users.noreply.github.com>
2021-08-06 23:21:00 +0000

    chore: removed nested 'package-lock' files (#1750)
    - remove nested 'package-lock.json' occurrences
    - remove unused 'react-html-attributes' in 'packages/forms'
    - add missing 'tslib' devDependency
    - change barrel export to named exports
    
    Signed-off-by: Vijay A <avijayr@protonmail.com>
    
    Co-authored-by: mhatvan <markus_hatvan@aon.at>

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,6 +1,6 @@
 export { Icon } from './icon';
 export { Infobox } from './infobox';
-export * from './headline';
+export { H1, H2, H3, H4 } from './headline';
 export { Loader } from './loader';
 export { Badge } from './badge';
 export { ProBadge } from './badge/ProBadge';
