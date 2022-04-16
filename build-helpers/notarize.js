086f5bd599ccbb305e83b5c6aae9a562dba67196
Vijay A <vraravam@users.noreply.github.com>
2022-04-16 23:22:55 +0530

    Changed license filename and the new license-holders

--- build-helpers/notarize.js
+++ build-helpers/notarize.js
@@ -10,7 +10,7 @@ exports.default = async function notarizing(context) {
   const { notarize } = require('electron-notarize');
 
   await notarize({
-    appBundleId: 'com.kytwb.ferdium',
+    appBundleId: 'org.ferdium.ferdium-app',
     appPath: `${appOutDir}/${appName}.app`,
     ascProvider: 'B6J9X9DWFL',
     appleId: process.env.APPLEID,
