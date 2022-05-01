3f90f2dd025572b6c06c609f7a800bc0561edd03
Vijay A <vraravam@users.noreply.github.com>
2022-04-30 21:41:49 -0500

    Fix issue where 'macos-notification-state' was being loaded in non-macos

--- src/electron/ipc-api/dnd.ts
+++ src/electron/ipc-api/dnd.ts
@@ -1,13 +1,19 @@
 import { ipcMain } from 'electron';
 import { isMac } from '../../environment';
 
-const { getDoNotDisturb } = require('macos-notification-state');
-
 const debug = require('../../preload-safe-debug')('Ferdium:ipcApi:dnd');
 
 export default async () => {
   ipcMain.handle('get-dnd', async () => {
     if (!isMac) {
+      return false;
+    }
+
+    // eslint-disable-next-line global-require
+    const { getDoNotDisturb } = require('macos-notification-state');
+
+    if (!getDoNotDisturb) {
+      debug("Could not load 'macos-notification-state' module");
       return false;
     }
 
