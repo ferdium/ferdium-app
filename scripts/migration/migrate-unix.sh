462d07993391dc044ed9541fccc578aceaa8094f
Evan-aja <gebel909@student.ub.ac.id>
2022-05-04 20:33:31 +0700

    requested changes

--- scripts/migration/migrate-unix.sh
+++ scripts/migration/migrate-unix.sh
@@ -2,9 +2,10 @@
 
 echo -e "********************************************"
 echo -e "      Ferdium User Data Migration Tool      "
-echo -e "          WARNING: Linux OS Only!           "
+echo -e "          WARNING: UNIX OS Only!            "
 echo -e "********************************************"
-echo -e "\n\nThis tool migrates your user data from Ferdi to Ferdium.\n\n"
+echo -e "\n\nThis tool migrates your user data from Ferdi to Ferdium."
+echo -e "Intended to be used on Linux or MacOS machine.\n\n"
 read -p "Do you want to proceed? (y/N) " confirm
 
 case "$confirm" in
@@ -13,12 +14,23 @@ case "$confirm" in
     * ) exit 1;;
 esac
 
-FERDI_PATH="$HOME/.config/Ferdi"
-FERDIUM_PATH="$HOME/.config/Ferdium"
+os_check=$(uname)
+echo "Your OS is $os_check"
 
-if [ -d $FERDIUM_PATH ]; then
+if [ $os_check == "Linux" ];then
+  FERDI_PATH="$HOME/.config/Ferdi"
+  FERDIUM_PATH="$HOME/.config/Ferdium"
+elif [ $os_check == "Darwin" ];then
+  FERDI_PATH="$HOME/Library/Application Support/Ferdi"
+  FERDIUM_PATH="$HOME/Library/Application Support/Ferdium"
+else
+  echo "Your OS is not supported by this script"
+  exit 1
+fi
+
+if [ -d "$FERDIUM_PATH" ]; then
     echo -e "\nPath $FERDIUM_PATH exist, making a backup\n"
-    if ! mv -vf $FERDIUM_PATH $FERDIUM_PATH.bak;then
+    if ! mv -vf "$FERDIUM_PATH" "$FERDIUM_PATH.bak" 2> /dev/null ;then
         read -p "A previous backup already exists at $FERDIUM_PATH.bak. do you want to remove it? (y/N) " confirm
         echo
         case "$confirm" in
@@ -27,13 +39,13 @@ if [ -d $FERDIUM_PATH ]; then
             * ) exit 1;;
         esac
         echo
-        rm -rf $FERDIUM_PATH.bak
-        mv -vf $FERDIUM_PATH $FERDIUM_PATH.bak
+        rm -rf "$FERDIUM_PATH.bak"
+        mv -vf "$FERDIUM_PATH" "$FERDIUM_PATH.bak"
     fi
 fi
 
-if mv -vf $FERDI_PATH $FERDIUM_PATH; then
-    cp -rf $FERDIUM_PATH $FERDI_PATH
+if mv -vf "$FERDI_PATH" "$FERDIUM_PATH"; then
+    cp -rf "$FERDIUM_PATH" "$FERDI_PATH"
     echo -e "\nFiles exported succesfully"
 else
     echo -e "\nERROR!"
