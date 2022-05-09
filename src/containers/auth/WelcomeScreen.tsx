253da3b2fb11e44f13773fbdfc75316400987719
Vijay A <vraravam@users.noreply.github.com>
2022-05-08 21:00:17 -0500

    Converted some javascript files to typescript

--- src/containers/auth/WelcomeScreen.tsx
+++ src/containers/auth/WelcomeScreen.tsx
@@ -1,12 +1,18 @@
 import { Component } from 'react';
-import PropTypes from 'prop-types';
 import { inject, observer } from 'mobx-react';
 
 import Welcome from '../../components/auth/Welcome';
 import UserStore from '../../stores/UserStore';
 import RecipePreviewsStore from '../../stores/RecipePreviewsStore';
 
-class WelcomeScreen extends Component {
+interface IProps {
+  stores: {
+    user: UserStore,
+    recipePreviews: RecipePreviewsStore,
+  },
+};
+
+class WelcomeScreen extends Component<IProps> {
   render() {
     const { user, recipePreviews } = this.props.stores;
 
@@ -20,12 +26,5 @@ class WelcomeScreen extends Component {
     );
   }
 }
-
-WelcomeScreen.propTypes = {
-  stores: PropTypes.shape({
-    user: PropTypes.instanceOf(UserStore).isRequired,
-    recipePreviews: PropTypes.instanceOf(RecipePreviewsStore).isRequired,
-  }).isRequired,
-};
 
 export default inject('stores', 'actions')(observer(WelcomeScreen));
