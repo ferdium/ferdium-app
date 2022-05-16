7eb03e3ce05b7677a76098d432be83d004e87a61
André Oliveira <oliveira.andrerodrigues95@gmail.com>
2022-05-16 17:26:26 +0100

    Fix #164

--- src/config.ts
+++ src/config.ts
@@ -170,7 +170,7 @@ export const DEFAULT_WINDOW_OPTIONS = {
 
 export const GITHUB_FRANZ_URL = 'https://github.com/meetfranz';
 export const GITHUB_FERDIUM_URL = 'https://github.com/ferdium';
-export const FRANZ_SERVICE_REQUEST = `${GITHUB_FERDIUM_URL}/recipes/issues`;
+export const FERDIUM_SERVICE_REQUEST = `${GITHUB_FERDIUM_URL}/ferdium-recipes/issues`;
 export const FRANZ_TRANSLATION = 'https://crowdin.com/project/ferdium';
 export const FRANZ_DEV_DOCS = 'http://bit.ly/franz-dev-hub';
 
