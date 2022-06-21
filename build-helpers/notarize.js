7122309df14f4854e31c6b09bb915d319bcd0aaa
Alphrag <34252790+Alphrag@users.noreply.github.com>
2022-06-21 01:17:33 +0100

    Update build process to sign mac releases

--- build-helpers/notarize.js
+++ build-helpers/notarize.js
@@ -12,7 +12,7 @@ exports.default = async function notarizing(context) {
   await notarize({
     appBundleId: 'org.ferdium.ferdium-app',
     appPath: `${appOutDir}/${appName}.app`,
-    ascProvider: 'B6J9X9DWFL',
+    ascProvider: '55E9FPJ93P',
     appleId: process.env.APPLEID,
     appleIdPassword: process.env.APPLEID_PASSWORD,
   });
