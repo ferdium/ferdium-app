6a7c4494dfdeb76ff17d12e39b8217bb23018254
Ricardo Cino <ricardo@cino.io>
2022-06-25 16:59:04 +0200

    chore: improve array-helper test

--- test/helpers/array-helpers.test.ts
+++ test/helpers/array-helpers.test.ts
@@ -2,6 +2,14 @@ import { shuffleArray } from '../../src/helpers/array-helpers';
 
 describe('array_helpers', () => {
   it('isValidExternalURL', () => {
-    expect(shuffleArray(['a', 'b', 'c'])).not.toEqual(['a', 'b', 'c']);
+    const originalArray = ['a', 'b', 'c'];
+    const shuffledArray = shuffleArray(originalArray);
+
+    // Expect the arrays to not be exactly the same
+    expect(shuffledArray).not.toEqual(originalArray);
+
+    // Expect the arrays to be exactly the same
+    // when both are sorted alphabetically
+    expect(shuffledArray.sort()).toEqual(originalArray.sort());
   });
 });
