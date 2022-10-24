2b9bd1f2963b313802b103bed6bb22736d452654
Muhamed <>
2022-10-25 03:08:37 +0530

    chore: update publish debug into to TS

--- src/features/publishDebugInfo/index.ts
+++ src/features/publishDebugInfo/index.ts
@@ -1,21 +1,17 @@
-import { state as ModalState } from './store';
+import { state } from './store';
 
 export { default as Component } from './Component';
 
-const state = ModalState;
 const debug = require('../../preload-safe-debug')(
   'Ferdium:feature:publishDebugInfo',
 );
 
-export default function initialize() {
+export default function initialize(): void {
   debug('Initialize publishDebugInfo feature');
 
-  function showModal() {
+  const showModal = (): void => {
     state.isModalVisible = true;
-  }
-
-  window['ferdium'].features.publishDebugInfo = {
-    state,
-    showModal,
   };
+
+  window['ferdium'].features.publishDebugInfo = { state, showModal };
 }
