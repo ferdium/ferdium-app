f1d8b48bee0c2f1fa8e5a2e5546f0f2418d0c40b
Nathanaël Houn <contact@nathanaelhoun.fr>
2023-02-15 20:28:19 +0100

    docs: update link for recipes issues in the app

--- src/config.ts
+++ src/config.ts
@@ -327,7 +327,7 @@ export const DEFAULT_WINDOW_OPTIONS = {
 
 export const GITHUB_FRANZ_URL = 'https://github.com/meetfranz';
 export const GITHUB_FERDIUM_URL = 'https://github.com/ferdium';
-export const FERDIUM_SERVICE_REQUEST = `${GITHUB_FERDIUM_URL}/ferdium-recipes/issues`;
+export const FERDIUM_SERVICE_REQUEST = `${GITHUB_FERDIUM_URL}/ferdium-app/issues`;
 export const FERDIUM_TRANSLATION = 'https://crowdin.com/project/ferdium-app';
 export const FRANZ_DEV_DOCS = 'http://bit.ly/franz-dev-hub';
 
