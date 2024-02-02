fbcdbec35d7f3701f343c7be1006dea6f1e2c118
Willy Woitas <dutscher_sbf@hotmail.com>
2024-02-02 23:04:39 +0100

    add token parse to notifications

--- src/webview/notifications.ts
+++ src/webview/notifications.ts
@@ -1,4 +1,4 @@
-import { ipcRenderer } from 'electron';
+import { ipcRenderer, clipboard } from 'electron';
 
 import { v4 as uuidV4 } from 'uuid';
 
@@ -13,6 +13,27 @@ export class NotificationsHandler {
       debug('New notification', title, options);
 
       const notificationId = uuidV4();
+
+      /*
+      parse the token digits from sms body, find "token" or "code" in options.body which reflect the sms content 
+      ---
+      Token: 03624 / SMS-Code = PIN Token
+      ---
+      Prüfcode 010313 für Microsoft-Authentifizierung verwenden.
+      ---
+      483133 is your GitHub authentication code. @github.com #483133
+      ---
+      eBay: Ihr Sicherheitscode lautet 080090. \nEr läuft in 15 Minuten ab. Geben Sie den Code nicht an andere weiter.
+      ---
+      PayPal: Ihr Sicherheitscode lautet: 989605. Geben Sie diesen Code nicht weiter.
+      */
+      const rawBody = options.body;
+      const { 0: token } = /\d{5,6}/.exec(options.body) || [];
+      if (token && (['token', 'code'].find(a => options.body.toLowerCase().includes(a)))) {
+        // with the extra "+ " it shows its copied to clipboard in the notification
+        options.body = "+ " + rawBody;
+        clipboard.writeText(token);
+      }
 
       ipcRenderer.sendToHost(
         'notification',
