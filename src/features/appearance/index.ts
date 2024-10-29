ba38913f222a205499cdfc217fb8dd84d97e042b
Denys Madureira <denysmb@zoho.com>
2024-10-29 23:19:39 +0000

    feat: make sidebar scrollable when in horizontal mode and increase padding size to 6px

--- src/features/appearance/index.ts
+++ src/features/appearance/index.ts
@@ -16,6 +16,12 @@ import { workspaceStore } from '../workspaces';
 
 const STYLE_ELEMENT_ID = 'custom-appearance-style';
 
+// The padding is 6px. We multiply because when we sum, we want to get
+// the value of the left and right side when using on width
+// and the value of the top and bottom side when using on height
+// So we get a total of 12px
+const PADDING = 6 * 2;
+
 const createStyleElement = () => {
   const styles = document.createElement('style');
   styles.id = STYLE_ELEMENT_ID;
@@ -234,7 +240,7 @@ const generateServiceRibbonWidthStyle = (
   return horizontal
     ? `
     .sidebar {
-      height: ${width + 8}px !important;
+      height: ${width + PADDING}px !important;
       overflow: hidden !important;
     }
     .sidebar div {
@@ -258,7 +264,7 @@ const generateServiceRibbonWidthStyle = (
       line-height: 0;
     }
     .app .app__content {
-      padding-top: ${width + sidebarSizeBias + 8}px !important;
+      padding-top: ${width + sidebarSizeBias + PADDING}px !important;
     }
     .workspaces-drawer {
       margin-top: -${width}px !important;
@@ -290,7 +296,7 @@ const generateServiceRibbonWidthStyle = (
       overflow: hidden !important;
     }
     .services__webview-wrapper {
-      padding: ${webviewPadding ? '4px' : '0px'};
+      padding: ${webviewPadding ? '6px' : '0px'};
     }
     .services__webview-wrapper webview {
       border-radius: ${webviewPadding ? '4px' : '0px'};
@@ -298,7 +304,7 @@ const generateServiceRibbonWidthStyle = (
   `
     : `
     .sidebar {
-      width: ${width + 8}px !important;
+      width: ${width + PADDING}px !important;
     }
     .tabs {
       justify-content: ${sidebarServicesAlignment};
@@ -319,7 +325,7 @@ const generateServiceRibbonWidthStyle = (
       width: calc(100% - ${300 + width}px) !important;
     }
     .services__webview-wrapper {
-      padding: ${webviewPadding ? '4px' : '0px'};
+      padding: ${webviewPadding ? '6px' : '0px'};
     }
     .services__webview-wrapper webview {
       border-radius: ${webviewPadding ? '4px' : '0px'};
