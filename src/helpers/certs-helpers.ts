7b275434a442957f4e5196ee42fc2102d5fbe596
Tomasz Duda <tomaszduda23@gmail.com>
2025-04-19 09:20:43 +0200

    support for Custom Root CA

--- src/helpers/certs-helpers.ts
+++ src/helpers/certs-helpers.ts
@@ -1,9 +1,10 @@
+import { X509Certificate } from 'node:crypto';
 import { join } from 'node:path';
 import { ensureDirSync, readFileSync, readdirSync } from 'fs-extra';
 import { userDataCertsPath } from '../environment-remote';
 import { removeNewLines } from '../jsUtils';
 
-export const checkIfCertIsPresent = (certData: string): boolean => {
+export const checkIfCertIsPresent = (clientCert): boolean => {
   const certsFolder = userDataCertsPath();
 
   ensureDirSync(certsFolder);
@@ -15,8 +16,19 @@ export const checkIfCertIsPresent = (certData: string): boolean => {
       encoding: 'utf8',
       flag: 'r',
     });
+    try {
+      const caCert = new X509Certificate(cert);
+      if (caCert.ca) {
+        const certToVerify = new X509Certificate(clientCert.issuerCert.data);
+        if (certToVerify.verify(caCert.publicKey)) {
+          return true;
+        }
+      }
+    } catch (error) {
+      console.error('Certificate verification error:', error);
+    }
     certs.push(removeNewLines(cert));
   }
 
-  return certs.length > 0 && certs.includes(removeNewLines(certData));
+  return certs.length > 0 && certs.includes(removeNewLines(clientCert.data));
 };
