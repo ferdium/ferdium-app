ad5bfedb986130fd5e681889390b888e1df317c7
Alphrag <34252790+Alphrag@users.noreply.github.com>
2025-05-22 18:15:11 +0100

    fix: services with self-signed certificates should not break

--- src/helpers/certs-helpers.ts
+++ src/helpers/certs-helpers.ts
@@ -5,29 +5,38 @@ import { ensureDirSync, readFileSync, readdirSync } from 'fs-extra';
 import { userDataCertsPath } from '../environment-remote';
 import { removeNewLines } from '../jsUtils';
 
+const debug = require('../preload-safe-debug')('Ferdium:App');
+
 export const checkIfCertIsPresent = (clientCert: Certificate): boolean => {
   const certsFolder = userDataCertsPath();
 
   ensureDirSync(certsFolder);
 
   const certs: string[] = [];
+  const clientCertHasCA: boolean = clientCert.issuerCert !== undefined;
+  let certToVerify: X509Certificate | undefined;
 
   try {
-    const certToVerify = new X509Certificate(clientCert.issuerCert.data);
+    if (clientCertHasCA) {
+      certToVerify = new X509Certificate(clientCert.issuerCert.data);
+    }
 
     for (const file of readdirSync(certsFolder)) {
       const cert = readFileSync(join(certsFolder, file), {
         encoding: 'utf8',
         flag: 'r',
       });
-      const caCert = new X509Certificate(cert);
-      if (caCert.ca && certToVerify.verify(caCert.publicKey)) {
-        return true;
+      if (clientCertHasCA) {
+        const caCert = new X509Certificate(cert);
+        if (caCert.ca && certToVerify?.verify(caCert.publicKey)) {
+          return true;
+        }
       }
+
       certs.push(removeNewLines(cert));
     }
   } catch (error) {
-    console.error(error);
+    debug('Error when parsing the certificates:', error);
   }
 
   return certs.length > 0 && certs.includes(removeNewLines(clientCert.data));
